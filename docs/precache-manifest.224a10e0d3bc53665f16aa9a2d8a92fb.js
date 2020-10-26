378b6283d98bd0e0464fff59a3d60a58d6762493
alejandromaselli <36821850+alejandromaselli@users.noreply.github.com>
2020-10-26 10:26:31 -0600

    docs agregado

---
+++ docs/precache-manifest.224a10e0d3bc53665f16aa9a2d8a92fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e6f555126166a13519b8e6ca2c0321",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c547260df1658957043d",
+    "url": "/static/css/main.df232d8a.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c547260df1658957043d",
+    "url": "/static/js/main.2e8c2650.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
