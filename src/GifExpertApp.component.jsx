68c988b0c5a8dcba037a5d0d57893129ce027461
alejandromaselli <36821850+alejandromaselli@users.noreply.github.com>
2020-10-26 10:25:25 -0600

    first commit

---
+++ src/GifExpertApp.component.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import AddCategory from './components/AddCategory';
+import GifGrid from './components/GifGrid';
+
+const GifExpertApp = () => {
+
+    const[categories, setCategories] = useState(['One Punch Man']);
+
+    return(
+        <div>
+            <h2 id='title'>GifExpertApp</h2>
+            <AddCategory 
+                setCategoris={ setCategories }
+                categoris={ categories }
+            />
+            <hr />
+            {
+                <ol>{
+                categories.map((element, index) => {
+                    return(
+                        <GifGrid 
+                            key={element} 
+                            category={element} 
+                        />
+                    )
+                })
+                }</ol>
+            }
+        </div>
+    );
+}
+
+export default GifExpertApp;
